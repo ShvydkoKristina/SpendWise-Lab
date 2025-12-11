fe8a7183a4f6547ecce45afccd75b3cad0f83137
ShvydkoKristina <havier8884+3@gmail.com>
2025-12-11 13:01:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Category = require('./Category');
-const Expense = require('./Expense');
-const DatabaseManager = require('./DatabaseManager');
-const Statistics = require('./Statistics');
+const Category = require("./Category");
+const Expense = require("./Expense");
+const DatabaseManager = require("./DatabaseManager");
+const Statistics = require("./Statistics");
 
 console.log("--- SpendWise System Started ---");
 
@@ -11,8 +11,8 @@ const stats = new Statistics(db);
 const food = new Category(1, "Їжа", "Green");
 const transport = new Category(2, "Транспорт", "Blue");
 
-db.saveExpense(new Expense(101, '2025-10-01', 150, food, "Обід"));
-db.saveExpense(new Expense(102, '2025-10-02', 40, transport, "Автобус"));
+db.saveExpense(new Expense(101, "2025-10-01", 150, food, "Обід"));
+db.saveExpense(new Expense(102, "2025-10-02", 40, transport, "Автобус"));
 
 console.log("\n--- Статистика ---");
 console.log("Всього витрачено: " + stats.calculateTotal() + " грн");
